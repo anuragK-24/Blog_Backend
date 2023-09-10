ab88e3a6b6d55ed1a810f526dac6cdfd274683a3
anuragK-24 <24sept.anurag@gmail.com>
2023-09-10 12:39:04 +0530

    Updated the code

--- index.js
+++ index.js
@@ -12,18 +12,12 @@ const userRoute = require("./routes/users");
 
 const postRoute = require("./routes/posts");
 
-const categoryRoute = require("./routes/categories");
-
-const multer = require("multer");
-
-const path = require("path");
 
 dotenv.config();
 app.use(express.json());
 app.use(cors());
 //by this we can send any .json file
 
-app.use("/images", express.static(path.join(__dirname, "/images")));
 
 const uri = process.env.MONGO_URL;
 
@@ -36,19 +30,7 @@ mongoose
     console.log(err);
   });
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
 
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("File has been uploaded");
-});
 
 app.use("/api/auth", authRoute);
 
@@ -56,7 +38,6 @@ app.use("/api/users", userRoute);
 
 app.use("/api/posts", postRoute);
 
-app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () => {
   // use "clg" to type following command
