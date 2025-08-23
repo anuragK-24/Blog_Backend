4aba98eadf7bdd457b6eba446cbbfe2565207497
anuragK-24 <24sept.anurag@gmail.com>
2025-08-23 17:38:05 +0530

    Updating index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ dotenv.config();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://blogspark-anuragk24.vercel.app/",
+    origin: "https://blogspark-anuragk24.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
